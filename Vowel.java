af80c7fc7a06f88e254f7fa079b110d6e6878213
Omghonge04 <omghonge4@gmail.com>
2025-12-25 21:21:39 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,36 @@
+// Take a single character input and check whether it is a vowel or consonant using a switch statement.
+
+
+public class Vowel {
+	public static void main(String[] args) {
+
+	char vowel = 'i';
+	switch(vowel) {
+	
+	case 'a':
+		System.out.println("vowel");
+		break;
+		
+	case 'e':
+		System.out.println("vowel");
+		break;
+	
+	case 'i':
+		System.out.println("vowel");
+		break;
+		
+	case 'o':
+		System.out.println("vowel");
+		break;
+		
+	case 'u':
+		System.out.println("vowel");
+		break;
+		
+		default:
+			System.out.println("consonant");
+	
+
+	}	            
+}
+}
