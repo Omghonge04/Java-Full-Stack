1a4d039aa3ad09e55442e7e318e81d58377b7def
Omghonge04 <omghonge4@gmail.com>
2025-12-25 21:33:08 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,21 @@
+//Use a switch statement to check if a number is even or odd (hint: use number % 2 as the expression)
+public class Evenorodd {
+	public static void main(String[] args) {
+		
+		int num = 5;
+		switch(num % 2) {
+		  
+		case 1: 
+			System.out.println("Odd");
+			break;
+			
+		case 2:
+			System.out.println("Even");
+			break;
+			
+		 
+		
+	}
+
+}
+}
