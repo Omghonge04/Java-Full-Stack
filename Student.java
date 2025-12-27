ee180ddaf064c73634454aa6e75b6e1eb330ddd0
Omghonge04 <omghonge4@gmail.com>
2025-12-27 20:46:42 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("Name : Om Ghonge");
+		System.out.println("Class : TE");
+		System.out.println("Branch : CSDS");
+
+	}
+
+}
