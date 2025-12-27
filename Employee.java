165a3ac3623a9d191c33810255e0d3517186e030
Omghonge04 <omghonge4@gmail.com>
2025-12-27 20:46:23 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,15 @@
+
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("Name of the Employee : Om Ghonge");
+		System.out.println("Employee ID : 23456");
+		System.out.println("Package : 5lpa");
+		System.err.println("Department : IT");
+		System.out.println("DOJ : 05/12/2025");
+	
+
+	}
+
+}
